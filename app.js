97f6dbf32500ccfc2d011c99992b6aae1d12ecd3
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-03-24 21:16:19 -0300

    Código por terminar

---
+++ app.js
@@ -0,0 +1,9 @@
+// 1. Captura de Elemento do Dom
+const inputDesc = document.querySelector('#desc-despesa');
+const inputValor = document.querySelector('#valor-gringo');
+const inputCotacao = document.querySelector('#valor-cotacao');
+const btnAdicionar =document.querySelector('btn-adicionar');
+const listaDespesasDOM = document.querySelector('#lista-despesas');
+const totalBrlDOM = document.querySelector('total-brl');
+
+//2. 
