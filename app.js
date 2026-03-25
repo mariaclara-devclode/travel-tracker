6e91b6677162f27d65038b81f1f894388e13e11e
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-03-25 19:25:20 -0300

    Lógica JS, processamento de Arrays e LocalStorage implementados

--- app.js
+++ app.js
@@ -1,71 +1,71 @@
 // 1. Captura de Elemento do Dom
-const inputDesc = document.querySelector('#desc-despesa');
-const inputValor = document.querySelector('#valor-gringo');
-const inputCotacao = document.querySelector('#valor-cotacao');
-const btnAdicionar =document.querySelector('#btn-adicionar');
-const listaDespesasDOM = document.querySelector('#lista-despesas');
-const totalBrlDOM = document.querySelector('#total-br');
+const inputDesc = document.querySelector("#desc-despesa");
+const inputValor = document.querySelector("#valor-gringo");
+const inputCotacao = document.querySelector("#valor-cotacao");
+const btnAdicionar = document.querySelector("#btn-adicionar");
+const listaDespesasDOM = document.querySelector("#lista-despesas");
+const totalBrlDOM = document.querySelector("#total-br");
 
 //2. Estado da Aplicação (Array de Objetos)
 // Tentamos buscar dados salvos previamente. Se não houver, iniciamos um Array vazio [].
-let despesas = JSON.parse(localStorage.getItem('viagem_despesas')) || [];
+let despesas = JSON.parse(localStorage.getItem("viagem_despesas")) || [];
 
-// 3. Função para Persistir Dados 
+// 3. Função para Persistir Dados
 const salvarNoLocalStorage = () => {
-    // O localStorage só aceita guardar textos (Strings).
-    // JSON.stringify converte nosso Array em formato de texto.
-    localStorage.setItem('viagem_despesas', JSON.stringify(despesas));
+  // O localStorage só aceita guardar textos (Strings).
+  // JSON.stringify converte nosso Array em formato de texto.
+  localStorage.setItem("viagem_despesas", JSON.stringify(despesas));
 };
 
 const adicionarDespesa = () => {
-    // O método .trim() remove espaços em branco acidentais no início e no fim do texto.
-    const desc = inputDesc.value.trim();
+  // O método .trim() remove espaços em branco acidentais no início e no fim do texto.
+  const desc = inputDesc.value.trim();
 
-    // parseFloat converte o texto digitado no input para um número decimal nativo do JS.
-    const valorOriginal = parseFloat(inputValor.value);
-    const cotacao = parseFloat(inputCotacao.value);
+  // parseFloat converte o texto digitado no input para um número decimal nativo do JS.
+  const valorOriginal = parseFloat(inputValor.value);
+  const cotacao = parseFloat(inputCotacao.value);
 
-
-// Validação de Dados (condicional)
-// isNaN significa "Is Not a Number" (Não é um número). Verifica se a conversão falhou.
-if (desc == "" || isNaN(valorOriginal) || isNaN(cotacao)){
-    alert("Por favor, preencha todos os campos corretamente com valores válidos.");
+  // Validação de Dados (condicional)
+  // isNaN significa "Is Not a Number" (Não é um número). Verifica se a conversão falhou.
+  if (desc == "" || isNaN(valorOriginal) || isNaN(cotacao)) {
+    alert(
+      "Por favor, preencha todos os campos corretamente com valores válidos."
+    );
 
     return; // Interrompe a execução da função imediatamente.
-}
+  }
 
-// Processamento Lógico (Operador Aritmétrico)
-const valorConvertidoBRL = valorOriginal * cotacao;
+  // Processamento Lógico (Operador Aritmétrico)
+  const valorConvertidoBRL = valorOriginal * cotacao;
 
- //Estrutura de Dados: Criando um objeto para representar umma única despesa
+  //Estrutura de Dados: Criando um objeto para representar umma única despesa
 
-    const novaDespesa = {
-        id: Date.now(), // Gera um número único baseado nos milissegundos atuais para indentificar o item 
-        descricao:desc,
-        valorEstrangeiro: valorOriginal,
-        valorReal: valorConvertidoBRL 
-    };
+  const novaDespesa = {
+    id: Date.now(), // Gera um número único baseado nos milissegundos atuais para indentificar o item
+    descricao: desc,
+    valorEstrangeiro: valorOriginal,
+    valorReal: valorConvertidoBRL,
+  };
 
-    // Modificando o Array e salvando
-despesas.push(novaDespesa);
-salvarNoLocalStorage();
+  // Modificando o Array e salvando
+  despesas.push(novaDespesa);
+  salvarNoLocalStorage();
 
-// Limpeza da Interface
-inputDesc.value = "";
-inputValor.value = "";
-inputCotacao.value = "";
-// Chama a função que vai desenhar a lista na tela (que criaremos no próximo passo)
-atualizarTela();
- };
-
+  // Limpeza da Interface
+  inputDesc.value = "";
+  inputValor.value = "";
+  inputCotacao.value = "";
+  // Chama a função que vai desenhar a lista na tela (que criaremos no próximo passo)
+  atualizarTela();
+};
 
 // Vinculando a ação ao clique do botão
-btnAdicionar.addEventListener('click', adicionarDespesa);
+btnAdicionar.addEventListener("click", adicionarDespesa);
 
 const atualizarTela = () => {
-    // 1. Gerando os itens da lista usando o método .map()
-    // O map percorre o Array e retorna um novo Array contendo os blocos de HTML.
-    const htmlDaLista = despesas.map(item => {
+  // 1. Gerando os itens da lista usando o método .map()
+  // O map percorre o Array e retorna um novo Array contendo os blocos de HTML.
+  const htmlDaLista = despesas.map((item) => {
     // O método .toFixed(2) garante a exibição com duas casas decimais.
 
     return `
@@ -78,21 +78,21 @@ const atualizarTela = () => {
 <span class="valor">R$ ${item.valorReal.toFixed(2)}</span>
 </li>
 `;
-});
-// O .join('') une todos os blocos gerados pelo map em um único texto contínuo,
+  });
+  // O .join('') une todos os blocos gerados pelo map em um único texto contínuo,
 
-// que é então injetado na tag <ul> do nosso HTML.
-listaDespesasDOM.innerHTML = htmlDaLista.join('');
+  // que é então injetado na tag <ul> do nosso HTML.
+  listaDespesasDOM.innerHTML = htmlDaLista.join("");
 
-// 2. Calculando o Total usando o método .forEach()
-let somaTotal = 0;
-// O forEach executa uma função para cada item, somando o valorReal à nossa variável.
-despesas.forEach(item => {
-somaTotal += item.valorReal; // Equivalente a: somaTotal = somaTotal
-+ item.valorReal
-});
-// Atualizando o elemento de texto no cabeçalho
-totalBrlDOM.textContent = `R$ ${somaTotal.toFixed(2)}`;
+  // 2. Calculando o Total usando o método .forEach()
+  let somaTotal = 0;
+  // O forEach executa uma função para cada item, somando o valorReal à nossa variável.
+  despesas.forEach((item) => {
+    somaTotal += item.valorReal; // Equivalente a: somaTotal = somaTotal
+    +item.valorReal;
+  });
+  // Atualizando o elemento de texto no cabeçalho
+  totalBrlDOM.textContent = `R$ ${somaTotal.toFixed(2)}`;
 };
 
 // Execução Inicial: Chamamos a função assim que o script carrega para renderizar
